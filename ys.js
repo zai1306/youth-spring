236c825c2c4ba984ea9079b55b4715b4698bde43
unknown <zai.ilyas1306@gmail.com>
2020-08-13 06:46:43 -0700

    youth-spring-finalized

--- ys.js
+++ ys.js
@@ -1,21 +1,22 @@
-// var swiper = new Swiper('.swiper-container', {
-//     pagination: {
-//       el: '.swiper-pagination',
-//       type: 'progressbar',
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 50)
+})
 
+const navbar = document.querySelector('.navbar')
 
-  window.addEventListener('scroll', function(){
-      var header = document.querySelector("header");
-      header.classList.toggle("sticky", window.scrollTo > 0)
+const navItem = document.querySelectorAll('.navbar li')
+
+navItem.forEach(function (nav) {
+  nav.addEventListener('click', (e) => {
+    const checkBx = document.querySelector('#check')
+    checkBx.checked = false
   })
 
 
 
-//  “Pakistan is proud of her youth, particularly the students who have always been in the forefront in the hour of trial and need. You are the nation’s leaders of tomorrow and you must fully equip yourself by discipline, education and training for the arduous task lying ahead of you. You should realise the magnitude of your responsibility and be ready to bear it.”
+})
+
+
+
 
